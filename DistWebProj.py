a0a296286111e881d433bbe0041c016f7a527c68
David Lewis <davlewis2011@gmail.com>
2016-11-14 15:14:18 -0500

    updated requirements.txt to include SQLalchemy and psycopg2

--- DistWebProj.py
+++ DistWebProj.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 # database URL format = databaseType://username:password@endpoint:port/DatabaseName
 # note: username and password is taken out for secure purposes. gotta figure out how to do environmental variables.
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{USERNAME}:{PASSWORD}@distweb.c7b6ayuxu8b8.us-east-2.rds.amazonaws.com:5432/{DATABASE NAME}'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dlewis:password@distweb.c7b6ayuxu8b8.us-east-2.rds.amazonaws.com:5432/distWebDB'
 
 database = SQLAlchemy(app)
 
@@ -17,10 +17,10 @@ def add_user():
 @app.route('/post_user', methods=['POST'])
 def post_user():
     from objects import User
-    userAccount = User(request.form['username'], request.form['password'])
-    database.session.add(userAccount)
-    database.session.commit()
-    return redirect(url_for('add_user'))
+    userAccount = User(request.form['username'], request.form['password']) ## grab the data from the form.
+    database.session.add(userAccount) # form the SQL to insert.
+    database.session.commit() # run the actual sql command.
+    return redirect(url_for('add_user')) # rever the user back to the  create user form.
 
 
 @app.route('/products')
