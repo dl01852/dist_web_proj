f56c33ea05fe01c9caa54d7fc9e653969a2c6138
David <davlewis2011@gmail.com>
2016-11-30 05:12:13 -0500

    scrapped

--- DistWebProj.py
+++ DistWebProj.py
@@ -28,7 +28,7 @@ def add_user():
                 if user == None or user.password != password:
                     login_error = "username or password is incorrect"
                 else:
-                    return render_template('HomePage.html',username=username)
+                    return render_template('ProductsPage.html',username=username)
         elif 'register' in request.form:
             username = request.form['username']
             password = request.form['password']
